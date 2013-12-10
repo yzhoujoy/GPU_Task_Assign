96004cd74417abf22651cd725caa965cfe65385e
YING ZHOU <yingzhou@resnet-59-88.resnet.ucsb.edu>
2013-12-10 09:24:30 -0800

    initial commit

---
+++ GPU_Task_Assign/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  GPU_Task_Assign
+//
+//  Created by YING ZHOU on 12/9/13.
+//  Copyright (c) 2013 YING ZHOU. All rights reserved.
+//
+
+
+#include <iostream>
+
+int main()
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
